81ab880d0679af78b787e81975cf4b69b9dd2468
khilnani <nik@khilnani.org>
2017-03-25 17:28:49 +0000

    updated

--- lpe/main.py
+++ lpe/main.py
@@ -4,6 +4,7 @@ import sys
 import os
 import json
 import argparse
+from pprint import pprint
 
 import boto3
 
@@ -18,15 +19,19 @@ instance_type = "t2.nano"
 session = None
 ec2 = None
 client = None
-key_pair_info = None
+key_pair = None
 instance_id = None
 instance_state = None
 
 parser = argparse.ArgumentParser(description='CLI to compile python dependencies for AWS Lambda')
 parser.add_argument('-p', '--profile', dest='aws_profile', metavar="AWS_PROFILE", help="AWS Profile")
+parser.add_argument('-k', '--key', dest='aws_key', metavar="AWS_KEY", help="Key name to use. Default: %s" % key_name)
+parser.add_argument('-t', '--type', dest='aws_type', metavar="AWS_INS_TYPE", help="Instance type. Default: %s" % instance_type)
 parser.add_argument('-d', '--dry-run', action='store_true', help="Dry run")
+parser.add_argument('-i', '--info', action='store_true', help="Instance info. Only IP. If verbose, prints additional info.")
 parser.add_argument('-s', '--stop', action='store_true', help="Stop when done")
-parser.add_argument('-t', '--terminate', action='store_true', help="Terminate instance and delete key")
+parser.add_argument('-r', '--remove', action='store_true', help="Remove all. Terminate instance and delete key")
+parser.add_argument('-v', '--verbose', action='store_true', help="Verbose")
 
 args = parser.parse_args()
 
@@ -40,26 +45,33 @@ if __name__ == '__main__':
     # KeyPair
     ###############################################
 
+    if args.aws_key:
+        key_name = args.aws_key
+
+    if args.aws_type:
+        key_name = args.aws_type
+
     try:
-        key_pair_info = ec2.KeyPair(key_name)
-        key_pair_info.load()
-        logger.info( "Using key with fingerprint:")
-        logger.info( key_pair_info.key_fingerprint )
+        key_pair = ec2.KeyPair(key_name)
+        key_pair.load()
+        logger.info( "Found key with fingerprint:")
+        logger.info( key_pair.key_fingerprint )
     except:
-        r = client.create_key_pair(KeyName=key_name)
         try:
+            r = client.create_key_pair(KeyName=key_name)
+            print( "New key created." )
+
             with open (key_name + '.pem', 'w') as f:
                 f.write(r['KeyMaterial'])
+            print( "Downloaded key." )
 
-            logger.info( "New key created." )
-
-            key_pair_info = ec2.KeyPair(key_name)
-            key_pair_info.load()
+            key_pair = ec2.KeyPair(key_name)
+            key_pair.load()
             logger.info( "Using key with fingerprint:")
-            logger.info( key_pair_info.key_fingerprint )
-        except:
-            client.delete_key_pair(KeyName=key_name)
-            logger.error("Unable to write key file. Deleted key.")
+            logger.info( key_pair.key_fingerprint )
+        except Exception as e:
+            print(e)
+            logger.error("Unable to create key.")
 
     ###############################################
     # Create Instance
@@ -71,17 +83,28 @@ if __name__ == '__main__':
             for ins in res['Instances']:
                 ins_id = ins['InstanceId']
                 ins_state = ins['State']['Name']
-                logger.info('Checking... Instance %s is %s' % (ins_id, ins_state))
+                print('Checking... Instance %s is %s' % (ins_id, ins_state))
                 if ins_state not in ('shutting-down', 'terminated'):
                     instance_id = ins_id
                     instance_state = ins_state
-                    logger.info("Instance %s is %s" % (instance_id, instance_state))
-    
+                    print("Instance %s is %s" % (instance_id, instance_state))
+                    if args.info:
+                        if args.verbose:
+                            print('Image Id: %s' % ins['ImageId'])
+                            print('Instance Id: %s' % ins['InstanceId'])
+                            print('Instance Type: %s' % ins['InstanceType'])
+                            print('Public IP: %s' % ins['PublicIpAddress'])
+                            print('Public DNS Name: %s' % ins['PublicDnsName'])
+                        else:
+                            print(ins['PublicDnsName'])
+
     if instance_id == None:
-        if args.terminate:
-            logger.info('No instance found. Not creating a new one.')
-            sys.exit()
+        if args.remove:
+            print('No instance found.')
+            sys.exit(0)
         else:
+            if args.info:
+                sys.exit(0)
             try:
                 logger.info('Creating a new instance.')
                 r = client.run_instances(
@@ -94,31 +117,31 @@ if __name__ == '__main__':
                         )
                 instance_id = r['Instances'][0]['InstanceId']
                 instance_state = r['Instances'][0]['State']['Name']
-                logger.info("Instance %s is %s" % (instance_id, instance_state))
+                print("Instance %s is %s" % (instance_id, instance_state))
             except:
-                logger.error("Unable to create AWS instance.")
+                print("Unable to create AWS instance.")
 
     if instance_state in ('shutting-down'):
         waiter = client.get_waiter('instance_terminated')
         waiter.wait(InstanceIds=[instance_id])
         instance_state = 'terminated'
-        logger.info('Instance %s terminated.' % (instance_id))
+        print('Instance %s terminated.' % (instance_id))
 
     if instance_state in ('terminated'):
-        logger.info('Instance %s terminated.' % (instance_id))
+        print('Instance %s terminated.' % (instance_id))
 
     if instance_state in ('stopping'):
         waiter = client.get_waiter('instance_stopped')
         waiter.wait(InstanceIds=[instance_id])
         instance_state = 'stopped'
-        logger.info('Instance %s stopped.' % (instance_id))
+        print('Instance %s stopped.' % (instance_id))
 
     if instance_state in ('stopped'):
-        if args.terminate:
+        if args.remove:
             r = client.terminate_instances(InstanceIds=[instance_id])
             if len(r['TerminatingInstances']) > 0:
                 instance_state = r['TerminatingInstances'][0]['CurrentState']['Name']
-                logger.info("Instance %s is %s" % (instance_id, instance_state))
+                print("Instance %s is %s" % (instance_id, instance_state))
             else:
                 logger.error('Instance %s was stopped, but could not be terminated' % (instance_id))
         else:
@@ -126,24 +149,24 @@ if __name__ == '__main__':
                 r = client.start_instances(InstanceIds=[instance_id])
                 if len(r['StartingInstances']) > 0:
                     instance_state = r['StartingInstances'][0]['CurrentState']['Name']
-                    logger.info("Instance %s is %s" % (instance_id, instance_state))
+                    print("Instance %s is %s" % (instance_id, instance_state))
                 else:
                     logger.error('Instance %s was stopped, but could not be started' % (instance_id))
 
     if instance_state in ('pending'):
         waiter = client.get_waiter('instance_running')
         waiter.wait(InstanceIds=[instance_id])
-        logger.info('Instance %s running.' % (instance_id))
+        print('Instance %s running.' % (instance_id))
 
 
-    if args.terminate:
+    if args.remove:
         r = client.terminate_instances(InstanceIds=[instance_id])
         if len(r['TerminatingInstances']) > 0:
             instance_state = r['TerminatingInstances'][0]['CurrentState']['Name']
-            logger.info("Instance %s is %s" % (instance_id, instance_state))
+            print("Instance %s is %s" % (instance_id, instance_state))
 
-            key_pair_info.delete()
-            logger.info('Deleted key.')
+            key_pair.delete()
+            print('Deleted key.')
         else:
             logger.error('Instance %s was stopped, but could not be terminated' % (instance_id))
 
@@ -153,15 +176,15 @@ if __name__ == '__main__':
         r = client.stop_instances(InstanceIds=[instance_id])
         if len(r['StoppingInstances']) > 0:
             instance_state = r['StoppingInstances'][0]['CurrentState']['Name']
-            logger.info("Instance %s is %s" % (instance_id, instance_state))
+            print("Instance %s is %s" % (instance_id, instance_state))
 
             if instance_state in ('stopping'):
                 waiter = client.get_waiter('instance_stopped')
                 waiter.wait(InstanceIds=[instance_id])
                 instance_state = 'stopped'
-                logger.info('Instance %s stopped.' % (instance_id))
+                print('Instance %s stopped.' % (instance_id))
             else:
-                logger.warning('Unexpected... Instance %s is %s' % (instance_id, instance_state))
+                logger.error('Unexpected... Instance %s is %s' % (instance_id, instance_state))
         else:
             logger.error('Instance %s was running, but could not be stopped' % (instance_id))
 
